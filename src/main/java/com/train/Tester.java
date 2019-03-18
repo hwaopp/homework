2aa2a7089027f25b8e8f3af3ced9a79b9617aa6c
hwaopp <henry8131joy@gmail.com>
2019-03-18 12:37:13 +0800

    master

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -1,4 +1,13 @@
 package com.train;
 
+import java.util.Scanner;
+
 public class Tester {
+    public static void main(String[] args) {
+        System.out.println("購買張數: ");
+        Scanner scanner = new Scanner(System.in);
+
+        Ticket ticket = new Ticket();
+        ticket.piece
+    }
 }
